39cb5ce5ae327b344616a85227a82c0bdb74c1c4
Gaboooou <gabriel.briones@alumnos.ucn.cl>
2025-10-14 21:59:11 -0300

    feat(Agregar Carrito de compras):Se agrego carrito de compra con varios cambios aparte, como la bd, DTO nuevos, modelos y Mappers. Cambio Program.cs

---
+++ src/Application/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,31 @@
+using TiendaUCN.src.Application.DTO.CartDTO;
+using TiendaUCN.src.Application.DTO.OrderDTO;
+using TiendaUCN.src.Application.DTO.ProductDTO;
+
+namespace TiendaUCN.src.Application.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        /// <summary>
+        /// Crea una nueva orden y vacía el carrito de compras.
+        /// </summary>
+        /// <param name="userId">Id del usuario autenticado</param>
+        /// <returns>Crea una nueva orden y vacía el carrito de compras.</returns>
+        Task<string> CreateAsync(int userId);
+
+        /// <summary>
+        /// Obtiene los detalles de una orden.
+        /// </summary>
+        /// <param name="orderCode">Código de la orden</param>
+        /// <returns>El detalle de la orden</returns>
+        Task<OrderDetailDTO> GetDetailAsync(string orderCode);
+
+        /// <summary>
+        /// Obtiene una lista de órdenes para un usuario específico.
+        /// </summary>
+        /// <param name="searchParams">Parámetros de búsqueda</param>
+        /// <param name="userId">Id del usuario al que pertenecen las órdenes</param>
+        /// <returns>Ordenes del usuario</returns>
+        Task<ListedOrderDetailDTO> GetByUserIdAsync(SearchParamsDTO searchParams, int userId);
+    }
+}
